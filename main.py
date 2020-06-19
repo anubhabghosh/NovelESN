a286197eaa92a92cbf2baf694ce2ba9bf0c20cb7
Aleix Espuña Fontcuberta <65950800+AleixEF@users.noreply.github.com>
2020-06-19 13:41:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -19,9 +19,52 @@ Copyright (C) 2020  Saikat Chatterjee and Aleix Espuña Fontcuberta
 import numpy as np
 import matplotlib.pyplot as plt
 from parser import define_parser
+from echo_state_network.novel import NovelEsn
 
 
 parser = define_parser()
 namespace = parser.parse_args()
-print(namespace)
 
+#the compulsary args
+train_file = namespace.training_file
+output_file = namespace.output_file
+tau = namespace.tau
+yntau_size_q = namespace.history_q
+
+#the optional args
+yn_size_p = namespace.history_p
+beta = namespace.beta
+test_file = namespace.test_file
+
+#loading the train and test data if any
+train_signal = np.loadtxt(train_file)
+if test_file:
+    test_signal = np.loadtxt(test_file)
+
+esn = NovelEsn(yn_size_p) #initialization of the esn
+esn.teacher_forcing(train_signal) #feedback about the train data
+
+prediction = esn.predict(tau, yntau_size_q, beta)[0] #pred of q values
+#given a training time series y_0, y_1, y_2 ...y_M-1, the program will predict:
+# y_M+tau-(q-1), y_M+tau-(q-2) ... y_M+tau
+np.savetxt(fname=output_file, X=prediction)
+
+index_last_pred = len(train_signal) + tau # = M+tau
+index_first_pred = index_last_pred-(yntau_size_q-1) # = M+tau-(q-1)
+pred_indexes = np.arange(index_first_pred, index_last_pred+1)
+
+#Training data plot
+plt.figure()
+plt.title("Training data vs time index")
+plt.plot(train_signal)
+
+#Prediction plot
+plt.figure()
+plt.title("Prediction vs time index")
+plt.plot(pred_indexes, prediction, label="prediction", color="green")
+if test_file:
+    plt.plot(pred_indexes, test_signal, label="test signal", color="orange")
+plt.legend()
+
+plt.show()
+    
