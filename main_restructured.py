bbc6f6d5ea5253b5e8a80cd2e9f26bc11068c2dc
Anubhab Ghosh <findghosh@gmail.com>
2020-12-16 14:35:07 +0100

    Fix different filenames while storing predictions

--- main_restructured.py
+++ main_restructured.py
@@ -34,17 +34,26 @@ from train_novelESN import train_model_ESN
 from train_linear_ar import train_model_AR
 from train_recurrent_nets import train_model_RNN
 
+def get_pred_output_file(filename, model_name):
+
+    filename_, extension = filename.split(".")
+    filename_ = filename_.lower()
+    if (not model_name is None) and (model_name in filename_):
+        filename_new = filename_ + "." + extension
+    elif not model_name is None:
+        filename_new = filename_ + "_" + model_name + "." + extension
+    return filename_new
+
 def main():
     
     parser = argparse.ArgumentParser(description=
     "Use a variety of recurrent architectures for predicting solar sunpots as a time series\n"\
-    "Example: python main_gs.py --model_type [esn/linear_ar/rnn/lstm/gru] --dataset dynamo --train_file [full path to training data file] \
-    --output_file [path to file containing predictions] --test_file [path to test file (if any)] \
-    --verbosity [1 or 2] \n"
+    "Example: python main_restructured.py --model_type [esn/linear_ar/rnn/lstm/gru] --dataset dynamo --train_file [full path to training data file] \
+    --output_file [path to file containing predictions] --predict_cycle_num [index of cycle to be predicted] --grid_search [0/1] --compare_all [0/1] \n"
     "Description of different model types: \n"\
     "esn: echo state network,\n" \
     "linear_ar: linear autoregressive model, \n"\
-    "rnn: simple recurrent network (vanilla RNN / Elman unit), \n" \
+    "rnn: simple recurrent network (vanilla RNN), \n" \
     "lstm: long-short term memory network, \n" \
     "gru: gated recurrent units (simplification of lstm architecture)", formatter_class=RawTextHelpFormatter)
 
@@ -52,8 +61,6 @@ def main():
     parser.add_argument("--dataset", help="Type of dataset used - (dynamo/solar_data/sinus)", default="dynamo", type=str)
     parser.add_argument("--train_file", help="Location of training data file", default=None, type=str)
     parser.add_argument("--output_file", help="Location of the output file", default=None, type=str)
-    parser.add_argument("--verbose", help="Verbosity (0 or 1)", default=0, type=int)
-    #parser.add_argument("--test_file", help="(Optional) Location of the test data file", default=None, type=str)
     parser.add_argument("--predict_cycle_num", help="Cycle index to be predicted", default=None, type=int)
     parser.add_argument("--grid_search", help="Option to perform grid search or not (1 - True, 0 - False)", default=0, type=int)
     parser.add_argument("--compare_all", help="Option to compare all models or not (1 - True, 0 - False)", default=0, type=int)
@@ -64,10 +71,8 @@ def main():
     dataset = args.dataset
     train_file = args.train_file
     output_file = args.output_file
-    verbose = args.verbose
     use_grid_search = args.grid_search
     compare_all_models = args.compare_all
-    # test_file = args.test_file
     predict_cycle_num = args.predict_cycle_num
 
     # Load the configurations required for training
@@ -90,6 +95,7 @@ def main():
     #TODO: Fix save model feature in case of comparison of all models
     if compare_all_models == 0:
 
+        output_file = get_pred_output_file(output_file, model_type)
         if model_type == "esn":
             predictions_esn = train_model_ESN(options, model_type, data, minimum_idx, predict_cycle_num=predict_cycle_num, 
                                             tau=1, output_file=output_file)
@@ -106,15 +112,15 @@ def main():
     elif compare_all_models == 1:
 
         predictions_esn, ytest = train_model_ESN(options, "esn", data, minimum_idx, predict_cycle_num=predict_cycle_num, 
-                                        tau=1, output_file=output_file)
+                                        tau=1, output_file=get_pred_output_file(output_file, "esn"))
         predictions_ar = train_model_AR(options, "linear_ar", data, minimum_idx, predict_cycle_num=predict_cycle_num, tau=1, 
-                                        output_file=output_file, use_grid_search=use_grid_search)
+                                        output_file=get_pred_output_file(output_file, "linear_ar"), use_grid_search=use_grid_search)
         #predictions_vanilla_rnn = train_model_RNN(options, "rnn", data, minimum_idx, predict_cycle_num=predict_cycle_num, tau=1, 
         #                                        output_file=output_file, use_grid_search=use_grid_search)
         predictions_lstm = train_model_RNN(options, "lstm", data, minimum_idx, predict_cycle_num=predict_cycle_num, tau=1, 
-                                        output_file=output_file, use_grid_search=use_grid_search)
+                                        output_file=get_pred_output_file(output_file, "lstm"), use_grid_search=use_grid_search)
         predictions_gru = train_model_RNN(options, "gru", data, minimum_idx, predict_cycle_num=predict_cycle_num, tau=1, 
-                                        output_file=output_file, use_grid_search=use_grid_search)
+                                        output_file=get_pred_output_file(output_file, "gru"), use_grid_search=use_grid_search)
 
         # Plot the LSTM, GRU predictions
         #plt.figure(figsize=(15,10))
