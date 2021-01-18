Fixed validation set computation similar to test set computation, modified tau for msah, add gsresults

armodel.py
@@ -31,7 +31,7 @@ class Linear_AR(nn.Module):
             
     def init_linearAR(self):
         self.net = nn.Sequential(
-            nn.Linear(self.num_taps, 1))
+            nn.Linear(self.num_taps, self.output_size))
         #print(self.net)
     
     def forward(self, inputs):

main_restructured.py
@@ -125,7 +125,7 @@ def main():
         
         tau_chosen = options["gru"]["output_size"]
         orig_stdout = sys.stdout
-        f_tmp = open('./results/compare_all_preds_{}_cycle{}_osize{}_logs_eps{}.txt'.format(dataset, predict_cycle_num, tau_chosen, options["gru"]["num_epochs"]), 'w')
+        f_tmp = open('./results/compare_all_preds_{}_cycle{}_osize{}_logs_eps{}_tr3.txt'.format(dataset, predict_cycle_num, tau_chosen, options["gru"]["num_epochs"]), 'w')
         sys.stdout = f_tmp
         
         #tau_chosen = options["gru"]["output_size"]
@@ -156,7 +156,7 @@ def main():
         compare_model_preds["pred_gru"] = list(np.float64(predictions_gru))
         print("GRU signal saved")
 
-        with open('./results/compare_all_preds_{}_cycle{}_osize{}_eps{}.json'.format(dataset, predict_cycle_num, tau_chosen, options["gru"]["num_epochs"]), 'w') as f:
+        with open('./results/compare_all_preds_{}_cycle{}_osize{}_eps{}_tr3.json'.format(dataset, predict_cycle_num, tau_chosen, options["gru"]["num_epochs"]), 'w') as f:
             f.write(json.dumps(compare_model_preds, cls=NDArrayEncoder, indent=2))
 
         # Plot the LSTM, GRU predictions

src/utils.py
@@ -158,7 +158,8 @@ def get_msah_training_dataset(X, minimum_idx, tau=1, p=np.inf):
         tmp = []
         if not np.isinf(p):
             # i spans the indexes of a cycle
-            for i in range(minimum_idx[icycle - 1], minimum_idx[icycle]):
+            # the shift is in accordance with the no. of steps ahead to predict
+            for i in range(minimum_idx[icycle - 1], minimum_idx[icycle], tau):
                 # tmp receives a tuple (training data, target)
                 if i - p >= 0 and (len(X[i:i + tau]) == tau):
                     # Append the p points prior to i

testrnn_aliter.py
@@ -180,19 +180,22 @@ def train_rnn(model, nepochs, tr_inputs, tr_targets, val_inputs, val_targets, tr
         losses.append(tr_loss.item())
 
         with torch.no_grad():
-            '''
+            
             _, P, _ = val_inputs.shape
-            val_predictions = predict_rnn(model=model, eval_input=tr_inputs[-1, :, :].reshape((1, P, -1)), n_predict=len(val_inputs))
-            print("Prediction size:{}".format(val_predictions.shape))
-            print("target size:{}".format(val_targets.shape))
-            val_loss = criterion(torch.FloatTensor(val_predictions).reshape((-1, 1)), val_targets)
+            #val_targets_flattened = torch.cat((val_targets[:, 0], val_targets[-1, 1:]))
+            val_targets_flattened = torch.flatten(val_targets)
+            val_predictions = predict_rnn(model=model, eval_input=tr_inputs[-1, :, :].reshape((1, P, -1)), n_predict=len(val_targets_flattened))
+            #print("Prediction size:{}".format(val_predictions.shape))
+            #print("target size:{}".format(val_targets_flattened.shape))
+            val_loss = criterion(torch.FloatTensor(val_predictions).reshape((-1, 1)), val_targets_flattened.reshape((-1, 1)))
             val_losses.append(val_loss.item())
+            
             '''
             X_val = Variable(val_inputs, requires_grad=False).type(torch.FloatTensor)
             val_predictions = model.forward(X_val)
             val_loss = criterion(val_predictions, val_targets)
             val_losses.append(val_loss.item())
-            
+            '''
 
         endtime = timer()
         # Measure wallclock time